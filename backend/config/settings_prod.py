9dc928add0f7f3afd55d6f35ca4d54b81f460d5d
flowtom <alaena@flowtomic.com>
2025-02-19 16:49:00 -0500

    chore: setting up environments

---
+++ backend/config/settings_prod.py
@@ -0,0 +1,7 @@
+from .settings import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['budgetslate.com']
+
+# Add any other production-specific settings here. 
---
+++ backend/config/settings_staging.py
@@ -0,0 +1,7 @@
+from .settings import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['staging.budgetslate.com']
+
+# Add any other staging-specific settings here. 
